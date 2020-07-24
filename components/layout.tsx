7f4550578a22a272d5302e373bd50599bee1701f
Ohád Viktor <ohad.viktor@danubiusinfo.hu>
2020-07-25 00:29:11 +0200

    initial setup

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,20 +1,17 @@
-import Head from 'next/head'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import Head from 'next/head';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Viktor Ohád';
+export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
+        <meta name="description" content="Learn how to build a personal website using Next.js" />
         <meta
           property="og:image"
           content={`https://og-image.now.sh/${encodeURI(
@@ -27,22 +24,14 @@ export default function Layout({ children, home }) {
       <header className={styles.header}>
         {home ? (
           <>
-            <img
-              src="/images/profile.jpg"
-              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-              alt={name}
-            />
+            <img src="/images/profile.jpg" className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`} alt={name} />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
           <>
             <Link href="/">
               <a>
-                <img
-                  src="/images/profile.jpg"
-                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                  alt={name}
-                />
+                <img src="/images/profile.jpg" className={`${styles.headerImage} ${utilStyles.borderCircle}`} alt={name} />
               </a>
             </Link>
             <h2 className={utilStyles.headingLg}>
@@ -62,5 +51,5 @@ export default function Layout({ children, home }) {
         </div>
       )}
     </div>
-  )
+  );
 }
---
+++ posts/bio.md
@@ -0,0 +1,49 @@
+---
+title: 'Bio'
+date: '2020-07-25'
+---
+
+### Hi there! 👋
+
+My name is Viktor Ohád and I'm a Full Stack developer who is interested in Functional programming, Web development and all kinds of software development topics in general.
+
+### My professional skills as a Software Developer
+
+Since I started my professsional career as a Frontend Developer I mainly posses skills and knowledge in the JavaScript/TypeScript ecosystem. However I'm not scared from Backend/DevOps (actually I'd like to become a DevOps engineer in the near future) tasks either.
+My current working tech stack is TypeScript + React + Redux (with Saga), C# .Net, Ms SQL, NHibernate, GIT, Azure + Azure DevOps in a Scrum setting.
+
+### Frontend
+
+- JavaScript (proficient)
+- TypeScript (proficient)
+- React (proficient)
+- HTML5 (proficient)
+- CSS3 (proficient)
+- Redux + Saga, thunk (proficient)
+- Vue / Angular (medium)
+
+### Backend
+
+- Node.js (proficient)
+- C# (medium)
+- .Net MVC, WebApi (medium)
+- SQL (medium)
+- NHibernate (beginner)
+- F# (beginner)
+
+### DevOps
+
+- Docker (medium)
+- Linux (medium)
+- Azure DevOps/Piplines/CI-CD (beginner)
+- Bash scripting (beginner)
+
+### General Software Development
+
+- GIT (proficient)
+- SCRUM (proficient)
+
+* 🔭 I’m currently working on a secret project which will help people learn programming
+* 🌱 I’m currently learning Functional programmin with F# and fp-ts
+* 📫 How to reach me: viktor.ohad@protonmail.com
+* ⚡ Fun fact: Love working out, cycling, soon to be biker
